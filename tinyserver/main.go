3da99db2b487db73be400d9ec18c93cebc7eb19d
yaeshimo <hatsushimoyae@gmail.com>
2019-01-11 00:53:03 +0900

    fix and add -template

--- tinyserver/main.go
+++ tinyserver/main.go
@@ -17,6 +17,39 @@ const (
 	DefaultAddr = ":8080"
 )
 
+type Config struct {
+	Addr string `json:"addr"`
+	Root string `json:"root"`
+	File string `json:"file"`
+}
+
+func (c *Config) ReadJSON(file string) error {
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, c)
+}
+
+func template(w io.Writer) error {
+	c := &Config{
+		Addr: "localhost:8080",
+		Root: "/path/to/srv/root/dir/",
+		File: "/path/to/file",
+	}
+	b, err := json.MarshalIndent(c, "", "  ")
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintln(w, string(b))
+	return err
+}
+
+const examples = `Examples:
+  $ cd "$srvroot" && ` + Name + `     # serve files
+  $ ` + Name + ` -addr localhost:8080 # listen only localhost
+`
+
 func main() {
 	var usageWriter io.Writer = os.Stderr
 	flag.Usage = func() {
@@ -30,27 +63,26 @@ func main() {
 		fmt.Fprintf(usageWriter, "Options:\n")
 		flag.PrintDefaults()
 		fmt.Fprintf(usageWriter, "\n")
-		fmt.Fprintf(usageWriter, "Examples:\n")
-		fmt.Fprintf(usageWriter, "  $ cd \"$srvroot\" && %s     # srve files\n", Name)
-		fmt.Fprintf(usageWriter, "  $ %s -addr localhost:8080 # listen only localhost\n", Name)
-		fmt.Fprintf(usageWriter, "\n")
+		fmt.Fprintf(usageWriter, "%s\n", examples)
 	}
 
 	var opt struct {
-		help    bool
-		version bool
+		help     bool
+		version  bool
+		template bool
 
 		config string
 
-		// marge to config, priority (opt > config)
+		// merge to config
 		root string
 		file string
 		addr string
 	}
 	flag.BoolVar(&opt.help, "help", false, "Display this message")
 	flag.BoolVar(&opt.version, "version", false, "Display version")
+	flag.BoolVar(&opt.template, "template", false, "Output config template to stdout")
 	flag.StringVar(&opt.config, "config", "", "Specify JSON format configuration file")
-	flag.StringVar(&opt.root, "root", "", "Specify serve root")
+	flag.StringVar(&opt.root, "root", "", "Specify serve root, defaults to current directory")
 	flag.StringVar(&opt.file, "file", "", "Specify serve file")
 	flag.StringVar(&opt.addr, "addr", DefaultAddr, "Specify listen address")
 	flag.Parse()
@@ -68,35 +100,50 @@ func main() {
 	case opt.version:
 		fmt.Printf("%s %s\n", Name, Version)
 		os.Exit(0)
-	}
-
-	// TODO: define type?
-	// for read json, priority (config < opt)
-	config := &struct {
-		File string
-		Root string
-		Addr string
-	}{
-		Addr: DefaultAddr,
-	}
-	readJSON := func(file string) error {
-		b, err := ioutil.ReadFile(file)
-		if err != nil {
-			return err
-		}
-		return json.Unmarshal(b, config)
-	}
-
-	if opt.config != "" {
-		if err := readJSON(opt.config); err != nil {
+	case opt.template:
+		if err := template(os.Stdout); err != nil {
 			log.Fatal(err)
 		}
+		os.Exit(0)
+	}
+
+	config := &Config{
+		Addr: DefaultAddr,
+	}
+	if opt.config != "" {
+		if err := config.ReadJSON(opt.config); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// merge from opt
+	if opt.addr != DefaultAddr {
+		config.Addr = opt.addr
 	}
 	if opt.root != "" {
 		config.Root = opt.root
 	}
-	if opt.addr != DefaultAddr {
-		config.Addr = opt.addr
+	if opt.file != "" {
+		config.File = opt.file
+	}
+
+	if config.Root != "" {
+		fi, err := os.Stat(config.Root)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if !fi.IsDir() {
+			log.Fatal("not a directory: " + config.Root)
+		}
+	}
+	if config.File != "" {
+		fi, err := os.Stat(config.File)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if !fi.Mode().IsRegular() {
+			log.Fatal("not a regular file: " + config.File)
+		}
 	}
 
 	var (
@@ -106,18 +153,10 @@ func main() {
 		srvHandler  = http.StripPrefix("/srv", http.FileServer(http.Dir(config.Root)))
 	)
 
-	// TODO: consider treat case of (opt.file != "" && config.Root != "")
-	if opt.file != "" {
-		// to os.Lstat?
-		f, err := os.Stat(opt.file)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if !f.Mode().IsRegular() {
-			log.Fatal("not regular file")
-		}
+	// TODO: consider treat case of (config.File != "" && config.Root != "")
+	if config.File != "" {
 		fileHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			http.ServeFile(w, r, opt.file)
+			http.ServeFile(w, r, config.File)
 		})
 		if config.Root == "" {
 			srvHandler = fileHandler
@@ -131,11 +170,10 @@ func main() {
 				h.ServeHTTP(w, r)
 			})
 	}
-
 	http.Handle("/", withLog(rootHandler))
 	http.Handle("/srv/", withLog(srvHandler))
 	http.Handle("/file", withLog(fileHandler))
 
-	log.Printf("Listen on %s\n", config.Addr)
+	log.Printf("Listen on %q\n", config.Addr)
 	log.Fatal(http.ListenAndServe(config.Addr, nil))
 }
